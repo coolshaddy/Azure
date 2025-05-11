eca014e2bed47f3357e499b07e947618cf150323
coolshaddy <coolshaddy81@gmail.com>
2025-05-12 01:17:55 +0530

    change

--- terraform-modules/monitoring/main.tf
+++ terraform-modules/monitoring/main.tf
@@ -10,7 +10,7 @@ resource "helm_release" "prometheus" {
   namespace  = "monitoring"
   create_namespace = true
   repository = "https://prometheus-community.github.io/helm-charts"
-  version    = "25.18.0"
+  version    = "15.0.0"
   values     = [file("${path.module}/prometheus-values.yaml")]
 }
 
@@ -20,6 +20,6 @@ resource "helm_release" "grafana" {
   namespace  = "monitoring"
   create_namespace = true
   repository = "https://grafana.github.io/helm-charts"
-  version    = "7.0.21"
+  version    = "6.7.4"
   values     = [file("${path.module}/grafana-values.yaml")]
 }
