f823fdaaa4d9f9a9d2b7a3cda94e9791dedc6245
coolshaddy <coolshaddy81@gmail.com>
2025-05-12 02:16:58 +0530

    updates

--- terraform-modules/backend-storage/main.tf
+++ terraform-modules/backend-storage/main.tf
@@ -1,19 +1,22 @@
-resource "azurerm_resource_group" "tfstate" {
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "tfstate" {
+resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = var.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_blob_public_access = false
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.tfstate.name
+  storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
