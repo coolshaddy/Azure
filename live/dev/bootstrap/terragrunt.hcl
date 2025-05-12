de63d0b277d6b0e3e3f05d93df973fe7b9bb8f32
coolshaddy <coolshaddy81@gmail.com>
2025-05-12 08:35:26 +0530

    updating bootstrap module

--- live/dev/bootstrap/terragrunt.hcl
+++ live/dev/bootstrap/terragrunt.hcl
@@ -6,5 +6,5 @@ inputs = {
   resource_group_name   = "virt-rg"
   storage_account_name  = "akstfstatestorage" # must be globally unique
   container_name        = "tfstate"
-  location              = "centralindia"
+  location              = "Central India"
 }
--- terraform-modules/backend-storage/outputs.tf
+++ terraform-modules/backend-storage/outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.tfstate.name
+  value = azurerm_resource_group.rg.name
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.tfstate.name
+  value = azurerm_storage_account.sa.name
 }
 
 output "container_name" {
